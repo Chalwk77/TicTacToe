7a9d0d09052d32bbbeae9c2af8414dc5eff21dec
Chalwk77 <jericho.crosby227@gmail.com>
2023-08-16 17:21:35 +1200

    ...

--- src/main/java/com/chalwk/game/GameOver.java
+++ src/main/java/com/chalwk/game/GameOver.java
@@ -54,18 +54,64 @@ public class GameOver {
     private static boolean checkWinner(Game game) {
         char[][] board = game.getBoard();
         int len = board.length;
-        int[][][] winningCombinations = new int[][][]{WINNING_COMBINATIONS[len - 3]};
-        for (int[][] winningCombination : winningCombinations) {
-            for (int[] combination : winningCombination) {
-                for (int i = 0; i < combination.length; i++) {
-                    int x = combination[i];
-                    if (board[x / len][x % len] != game.player1 && board[x / len][x % len] != game.player2) {
-                        break;
-                    }
-                    if (i == combination.length - 1) {
-                        return true;
-                    }
-                }
+        switch (len) {
+            case 3 -> {
+                return board1(board, game);
+            }
+            case 4 -> {
+                return board2(board, game);
+            }
+            case 5 -> {
+                return board3(board, game);
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean board1(char[][] board, Game game) {
+        for (int[] combination : WINNING_COMBINATIONS[0]) {
+            int x = combination[0];
+            int y = combination[1];
+            int z = combination[2];
+            if (board[x / 3][x % 3] == game.symbol
+                    && board[y / 3][y % 3] == game.symbol
+                    && board[z / 3][z % 3] == game.symbol) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean board2(char[][] board, Game game) {
+        for (int[] combination : WINNING_COMBINATIONS[1]) {
+            int x = combination[0];
+            int y = combination[1];
+            int z = combination[2];
+            int w = combination[3];
+            if (board[x / 4][x % 4] == game.symbol
+                    && board[y / 4][y % 4] == game.symbol
+                    && board[z / 4][z % 4] == game.symbol
+                    && board[w / 4][w % 4] == game.symbol) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean board3(char[][] board, Game game) {
+        for (int[] combination : WINNING_COMBINATIONS[2]) {
+            int x = combination[0];
+            int y = combination[1];
+            int z = combination[2];
+            int w = combination[3];
+            int v = combination[4];
+            if (board[x / 5][x % 5] == game.symbol
+                    && board[y / 5][y % 5] == game.symbol
+                    && board[z / 5][z % 5] == game.symbol
+                    && board[w / 5][w % 5] == game.symbol
+                    && board[v / 5][v % 5] == game.symbol) {
+                return true;
             }
         }
         return false;
